82556f811f05e996e014f461d43e2c652ae4d43c
Andrey Pavlov <andrey.mp@gmail.com>
2017-11-25 16:07:53 +0300

    do not remove SConscript for base/test

--- scripts/03/run.sh
+++ scripts/03/run.sh
@@ -22,10 +22,6 @@ ln -s $CONTRAIL_BUILDROOT_DIR buildroot
 mkdir -p third_party
 ln -s $CONTRAIL_BUILD_DIR/third_party/fysom-1.0.8 third_party/fysom-1.0.8
 
-# tests don't work - remove SCons
-# TODO: make different targets for test and install
-rm -f src/contrail-common/base/test/SConscript
-
 mkdir -p controller/src
 pushd controller/src
 ln -s ../../src/contrail-common/base base
@@ -52,6 +48,9 @@ mkdir -p build/debug/sandesh/common/dist
 
 scons --root=$CONTRAIL_BUILDROOT_DIR install
 scons --root=$CONTRAIL_BUILDROOT_DIR install sandesh:test
+
+rm -rf controller-template src tools
+
 popd
 
 tar -czPf step-3.tgz $CONTRAIL_BUILD_DIR $CONTRAIL_BUILDROOT_DIR
