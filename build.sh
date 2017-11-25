4e3b2d77478874191a1c5b923112e0d44cd302f3
Andrey Pavlov <andrey.mp@gmail.com>
2017-11-25 16:34:30 +0300

    add build-full script

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+my_file="$(readlink -e "$0")"
+my_dir="$(dirname $my_file)"
+
+touch $HOME/build.log
+for item in '01' '02' '03' '04' '05' '06' '10' ; do
+  echo "INFO: start time of step $item  $(date)" | tee $HOME/build.log
+  cat $my_dir/scripts/$item/descript.ion | tee $HOME/build.log
+  $my_dir/scripts/$item/run.sh
+  echo "INFO: end time of step $item  $(date)" | tee $HOME/build.log
+done
