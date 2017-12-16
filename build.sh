a566c2ba0160c3c90c7b8f0a7cfaf1f606eefb22
Andrey Pavlov <andrey.mp@gmail.com>
2017-12-16 15:14:32 +0300

    try to freeze repos on 2017-12-10 00:00:00

--- build.sh
+++ build.sh
@@ -3,24 +3,17 @@
 my_file="$(readlink -e "$0")"
 my_dir="$(dirname $my_file)"
 
-export OPENSTACK_VERSION=${OPENSTACK_VERSION:-"newton"}
-export CONTRAIL_VERSION=${CONTRAIL_VERSION:-"4.2.0.0-1"}
-export CONTRAIL_RELEASE=`echo $CONTRAIL_VERSION | cut -d '-' -f 1`
-export CONTRAIL_BUILD=`echo $CONTRAIL_VERSION | cut -d '-' -f 2`
-export WORKSPACE=${WORKSPACE:-$HOME}
-logdir="$WORKSPACE/logs/"
-mkdir -p $logdir
+export WORKSPACE=$(pwd)
 
-test -f $WORKSPACE/reporevisions && rm -f $WORKSPACE/reporevisions
-for i in `grep -rh "gitclone" $my_dir/scripts/* | grep -Eo "https://[a-zA-Z0-9./?=_-]*" | sort | uniq` ; do
-   r=$(git ls-remote $i refs/heads/master | awk '{print $1}')
-   echo "$i $r" >> $WORKSPACE/reporevisions
-done
+if [ ! -f $WORKSPACE/reporevisions ] ; then
+  for i in `grep -rh "gitclone" $my_dir/scripts/* | grep -Eo "https://[a-zA-Z0-9./?=_-]*" | sort | uniq` ; do
+    r=$(git ls-remote $i refs/heads/master | awk '{print $1}')
 
-touch $logdir/build.log
-for item in '01' '02' '03' '04' '05' '06' '10' '11' ; do
-  echo "INFO: start time of step $item  $(date)" | tee -a $logdir/build.log
-  cat "$my_dir/scripts/$item/descript.ion" | tee -a $logdir/build.log
-  $my_dir/scripts/$item/run.sh
-  echo "INFO: end time of step $item  $(date)" | tee -a $logdir/build.log
-done
+    # for cloned repos we can get SHA by date:
+    #pushd $(echo $i | awk -F"/" 'NF>1{print $NF}')
+    #r=$(git rev-list -n 1 --before="2017-12-10 00:00:00" master)
+    #popd
+
+    echo "$i $r" >> $WORKSPACE/reporevisions
+  done
+fi
